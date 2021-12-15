a8240fe3cf369b5b496423d51f35485f4f1ed4ca
Lequilong <longcode2504@gmail.com>
2021-12-15 21:27:15 +0700

    responsive xong

--- access/Table/main.js
+++ access/Table/main.js
@@ -48,16 +48,16 @@ $(document).ready(function(){
     });
   });
 
-$(document).ready(function(){
-    $('.content-header-discount-body-list').slick({
-        slidesToShow: 6,
-        slidesToScroll: 6,
-        infinite: false,
-        arrows: true,
-        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='btn_prev ti-angle-left'></i></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right'><i class='btn_prev ti-angle-right'></i></button>"
-    });
-});
+// $(document).ready(function(){
+//     $('.content-header-discount-body-list').slick({
+//         slidesToShow: 6,
+//         slidesToScroll: 6,
+//         infinite: false,
+//         arrows: true,
+//         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='btn_prev ti-angle-left'></i></button>",
+//         nextArrow:"<button type='button' class='slick-next pull-right'><i class='btn_prev ti-angle-right'></i></button>"
+//     });
+// });
 
 
 $(document).ready(function(){
@@ -74,25 +74,188 @@ $(document).ready(function(){
     });
   });
 
-$(document).ready(function(){
-    $('.content_ccontainer-BrandCardsWrapper-c1').slick({
-        slidesToShow: 6,
+// $(document).ready(function(){
+//     $('.content_ccontainer-BrandCardsWrapper-c1').slick({
+//         slidesToShow: 6,
+//         slidesToScroll: 2,
+//         infinite: false,
+//         arrows: true,
+//         prevArrow:"<button type='button' class='slick-prev pull-left lino-icon-left'><ion-icon name='chevron-back-outline'></ion-icon></button>",
+//         nextArrow:"<button type='button' class='slick-next pull-right lino-icon-right'><ion-icon name='chevron-forward-outline'></ion-icon></button>"
+//     });
+// });
+
+// $(document).ready(function(){
+//     $('.content_container-top_trending-body-item').slick({
+//         slidesToShow: 4,
+//         slidesToScroll: 4,
+//         infinite: false,
+//         arrows: true,
+//         prevArrow:"<button type='button' class='slick-prev pull-left lino-icon-left'><ion-icon name='chevron-back-outline'></ion-icon></button>",
+//         nextArrow:"<button type='button' class='slick-next pull-right lino-icon-right'><ion-icon name='chevron-forward-outline'></ion-icon></button>"
+//     });
+// });
+
+$('.content_container-tab-category').slick({
+    infinite: false,
+    slidesToShow: 10,
+    slidesToScroll: 1,
+    respondTo:'slider',
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }
+   
+    ]
+});
+
+$('.content_ccontainer-BrandCardsWrapper-c1').slick({
+  infinite: false,
+  slidesToShow: 6,
+  slidesToScroll: 2,
+  respondTo:'slider',
+  arrows: true,
+  prevArrow:"<button type='button' class='slick-prev pull-left lino-icon-left'><ion-icon name='chevron-back-outline'></ion-icon></button>",
+  nextArrow:"<button type='button' class='slick-next pull-right lino-icon-right'><ion-icon name='chevron-forward-outline'></ion-icon></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
+});
+
+$('.content_container-table-lazyload-slider-c1').slick({
+  centerMode: true,
+  centerPadding: '50px',
+  slidesToShow: 1,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed : 3000,
+  dotsClass: 'slider-table',
+  adaptiveHeight: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '50px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+$('.content_container-top_trending-body-item').slick({
+  infinite: false,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  respondTo:'slider',
+  arrows: true,
+  prevArrow:"<button type='button' class='slick-prev pull-left lino-icon-left'><ion-icon name='chevron-back-outline'></ion-icon></button>",
+  nextArrow:"<button type='button' class='slick-next pull-right lino-icon-right'><ion-icon name='chevron-forward-outline'></ion-icon></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
         slidesToScroll: 2,
-        infinite: false,
-        arrows: true,
-        prevArrow:"<button type='button' class='slick-prev pull-left lino-icon-left'><ion-icon name='chevron-back-outline'></ion-icon></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right lino-icon-right'><ion-icon name='chevron-forward-outline'></ion-icon></button>"
-    });
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+  ]
 });
 
-$(document).ready(function(){
-    $('.content_container-top_trending-body-item').slick({
+$('.content-header-discount-body-list').slick({
+  infinite: false,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  respondTo:'slider',
+  arrows: true,
+  prevArrow:"<button type='button' class='slick-prev pull-left lino-icon-left'><ion-icon name='chevron-back-outline'></ion-icon></button>",
+  nextArrow:"<button type='button' class='slick-next pull-right lino-icon-right'><ion-icon name='chevron-forward-outline'></ion-icon></button>",
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 6,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
         slidesToShow: 4,
-        slidesToScroll: 4,
-        infinite: false,
-        arrows: true,
-        prevArrow:"<button type='button' class='slick-prev pull-left lino-icon-left'><ion-icon name='chevron-back-outline'></ion-icon></button>",
-        nextArrow:"<button type='button' class='slick-next pull-right lino-icon-right'><ion-icon name='chevron-forward-outline'></ion-icon></button>"
-    });
-});
+        slidesToScroll: 4
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    }
 
+  ]
+});
